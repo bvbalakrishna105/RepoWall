4c96a7724d68e7a18d5314527909c474a0b82012
Balu <bvbalakrishna.iitg@gmail.com>
2014-08-30 02:08:06 +0200

    Added new files

---
+++ Platform/Database/src/ReadDataBaseConfing.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+import java.io.File;
+import javax.swing.text.View;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+
+public class ReadDataBaseConfing {
+
+  
+}
---
+++ Platform/Database/src/sample.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrator
+ */
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class sample {
+    public static void main(String[] args) {
+        try{
+            
+            
+            File fileConfig = new File("..//Settings//config.xml");
+            
+            if(!fileConfig.isFile()){
+                return;
+            }
+           
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fileConfig);
+            doc.getDocumentElement().normalize();
+            
+            System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+            
+            NodeList nList = doc.getElementsByTagName("database");
+            
+            System.out.println("----------------------------");
+            
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+ 
+		Node nNode = nList.item(temp);
+ 
+		System.out.println("\nCurrent Element :" + nNode.getNodeName());
+ 
+		if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+ 
+			Element eElement = (Element) nNode;
+                        String colonDelimenator = ":";
+                        String forwardSlash = "/";
+                        String databaseConnectionString = eElement.getElementsByTagName("url").item(0).getTextContent();                      
+                        String ipAddress = eElement.getElementsByTagName("ipaddress").item(0).getTextContent();
+                        String portNumber = eElement.getElementsByTagName("port").item(0).getTextContent();
+                        String databaseName = eElement.getElementsByTagName("databaseName").item(0).getTextContent();
+                        databaseConnectionString = databaseConnectionString.concat(ipAddress).concat(colonDelimenator).concat(portNumber).concat(forwardSlash).concat(databaseName);
+                        
+			System.out.println("Database : " + eElement.getAttribute("name"));
+			System.out.println("databasedriver : " + eElement.getElementsByTagName("databasedriver").item(0).getTextContent());
+                        System.out.println(databaseConnectionString);
+                        System.out.println("username : " + eElement.getElementsByTagName("username").item(0).getTextContent());
+                        System.out.println("password : " + eElement.getElementsByTagName("password").item(0).getTextContent());
+		}
+	}
+            
+        }catch(Exception ee){
+            ee.getStackTrace();
+        }
+        
+    }
+    
+}
