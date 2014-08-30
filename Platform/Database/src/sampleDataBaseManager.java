cb2d4c1b547682499923b1846ee9f2b804c11ead
Balu <bvbalakrishna.iitg@gmail.com>
2014-08-30 13:31:53 +0200

    Added Source code Licence test

--- Platform/Database/src/sampleDataBaseManager.java
+++ Platform/Database/src/sampleDataBaseManager.java
@@ -1,13 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
+/****************************************************************************/
+/*!
  *
- * @author Administrator
+ *
+ *  $Version:   $ 0.1
+ *  $Date:      $ 2014-08-30
+ *  $Author:    $ Venkata Balakrushna Beesetti
+ *
+ *  \b Company:
+ *
+ *       RakutWall Pvt.Ltd
+ *
+ *  (C) Copyright 2011 by RakutWall Pvt. Ltd. All rights reserved.
+ *  This is unpublished proprietary source code of Leica. The copyright notice
+ *  does not evidence any actual or intended publication.
+ *
  */
+/****************************************************************************/
 public class sampleDataBaseManager {
     
     
